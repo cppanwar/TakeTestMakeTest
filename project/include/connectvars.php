7261779f996e53fc76968caf586e78f5d831b64e
cppanwar <cppanwar1@gmail.com>
2018-08-12 20:07:02 +0530

    Delete connectvars.php

--- project/include/connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'example.com');
-  define('DB_USER', 'example_user');
-  define('DB_PASSWORD', 'example password');
-  define('DB_NAME', 'example_database');
-?>
